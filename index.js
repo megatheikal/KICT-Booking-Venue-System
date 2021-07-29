bfb7198a5aedf0a15f7b0f24b1eaa2490abc470c
m00m <mgaat.hkl@gmail.com>
2021-07-29 17:26:25 +0800

    Connect Postgresql with Hapi js

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
+const env = require("env2")("env.json");
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  await server.register({
+    plugin: require("hapi-pgsql"),
+    options: {
+      database_url: process.env.db_url,
+    },
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: async (request, h) => {
+      const time = await request.pgsql.query("SELECT NOW()");
+      return `Hello World! Time: ${time.rows[0].now}`;
+    },
+  });
 
-            return 'Hello World!';
-        }
-    });
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
